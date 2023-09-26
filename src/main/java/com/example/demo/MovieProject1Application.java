65ac01d0aaf27e599ba7863dbf4932d6c1c8962f
NEC\naya.shih <naya.shih@nec.com.tw>
2023-09-26 18:31:39 +0800

    test

--- src/main/java/com/example/demo/MovieProject1Application.java
+++ src/main/java/com/example/demo/MovieProject1Application.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class MovieProject1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MovieProject1Application.class, args);
 	}
